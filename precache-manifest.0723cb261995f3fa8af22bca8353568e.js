a204e10fd0c00da7e2cb0dd7b133a4987189dfc5
Tolokoban <contact@tolokoban.org>
2019-11-11 23:00:55 +0100

    Updates

---
+++ precache-manifest.0723cb261995f3fa8af22bca8353568e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332440b5928a021c122768046ccc7920",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c610c6b001737e1d7a25",
+    "url": "./static/css/index.58b687bf.3acec90f.chunk.css"
+  },
+  {
+    "revision": "428951d25eb689e829df",
+    "url": "./static/css/index.731f7d27.01bfa036.chunk.css"
+  },
+  {
+    "revision": "4a45cb9e3262f29b41c9",
+    "url": "./static/css/index.85478ee2.71184cb6.chunk.css"
+  },
+  {
+    "revision": "0fa17a826314d12105e7",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2840140dae19cefd23ae",
+    "url": "./static/js/0.754736d4.chunk.js"
+  },
+  {
+    "revision": "80c5621bcf2657f44e34",
+    "url": "./static/js/1.1da77bf4.chunk.js"
+  },
+  {
+    "revision": "c610c6b001737e1d7a25",
+    "url": "./static/js/index.58b687bf.247b3c4d.chunk.js"
+  },
+  {
+    "revision": "428951d25eb689e829df",
+    "url": "./static/js/index.731f7d27.c757bdb9.chunk.js"
+  },
+  {
+    "revision": "4a45cb9e3262f29b41c9",
+    "url": "./static/js/index.85478ee2.6308998c.chunk.js"
+  },
+  {
+    "revision": "0fa17a826314d12105e7",
+    "url": "./static/js/main.15cb8975.chunk.js"
+  },
+  {
+    "revision": "9f83267410c42ffa8d85",
+    "url": "./static/js/runtime-index.58b687bf.a73a0440.js"
+  },
+  {
+    "revision": "d77847b59b31a2c646c1",
+    "url": "./static/js/runtime-index.731f7d27.a5b4b72f.js"
+  },
+  {
+    "revision": "5d3fa59adc1863a5c8da",
+    "url": "./static/js/runtime-index.85478ee2.7eb684df.js"
+  },
+  {
+    "revision": "aa85cead787c205e54c1",
+    "url": "./static/js/runtime-main.999881b1.js"
+  },
+  {
+    "revision": "36c1c0819ed882b9eaf90e6c2c80d34c",
+    "url": "./static/media/atlas.36c1c081.png"
+  },
+  {
+    "revision": "086dcaef6afa84ebf4d12a77af999066",
+    "url": "./static/media/background.086dcaef.jpg"
+  },
+  {
+    "revision": "0935ef3449b4dbd252fb283b30808c78",
+    "url": "./static/media/background.0935ef34.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.268242c9552df38a23907866b0264b54.js"
+  "./precache-manifest.0723cb261995f3fa8af22bca8353568e.js"
 );
 
 self.addEventListener('message', (event) => {
