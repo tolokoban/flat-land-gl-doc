a35960eb05a21428e2f1cfaf7812376a156dc53e
Tolokoban <contact@tolokoban.org>
2019-11-11 22:57:56 +0100

    New tutorial with a lots of sprites.

--- src/page/tutorial/sprites-1/index.tsx
+++ src/page/tutorial/sprites-1/index.tsx
@@ -5,7 +5,7 @@ import FlatLand from 'flat-land-gl'
 import Canvas from '../../../canvas'
 //import Description from '../../../view/description'
 //import description from './sprites-1.md'
-import bowlAtlas from './bowl.png'
+import atlasAtlas from './atlas.png'
 import backgroundAtlas from './background.png'
 
 
@@ -16,10 +16,41 @@ ReactDOM.render(<div>
 
 
 async function init(canvas: HTMLCanvasElement) {
+    function newType(x: number, y: number, w: number, h: number) {
+        return {
+            width: w,
+            height: h,
+            u0: x / 524,
+            v0: y / 856,
+            u1: (x + w) / 524,
+            v1: (y + h) / 856
+        }
+    }
+    const TYPES: {[key: string]: any} = {
+        bol: newType(0, 0, 542, 318),
+        corbeille: newType(0, 320, 402, 524),
+        tasse: newType(401, 467, 122, 119),
+        citron: newType(405, 675, 117, 110),
+        croissant: newType(415, 785, 104, 72),
+        framboise: newType(400, 617, 61, 58),
+        noisette: newType(462, 589, 61, 85),
+        the: newType(402, 316, 124, 149)
+    }
+
+    function add(items: any[], name: string, count: number = 1) {
+        for (let k=0 ; k<count ; k++) {
+            const type = TYPES[name]
+            const item = sprites.createSprite(type)
+            item.extra.speed1 = (1 + Math.random()) * 0.001
+            item.extra.speed2 = (1 + Math.random()) * 0.001
+            items.push(item)
+        }
+    }
+
     const scene = new FlatLand.Scene(canvas)
-    await scene.createAtlas({
-        name: "bowl",
-        image: bowlAtlas
+    scene.createAtlas({
+        name: "atlas",
+        image: atlasAtlas
     })
     scene.createAtlas({
         name: "background",
@@ -32,28 +63,31 @@ async function init(canvas: HTMLCanvasElement) {
     })
     const sprites = new FlatLand.Painter.Sprites({
         scene,
-        atlas: "bowl"
+        atlas: "atlas"
     })
 
-    const bowls: any[] = []
-    for (let count=0 ; count<7 ; count++) {
-        const bowl = sprites.createSprite({
-            x: 500, y: 500 })
-        bowls.push(bowl)
-    }
+    const items: any[] = []
+    add(items, "bol")
+    add(items, "corbeille")
+    add(items, "tasse", 12)
+    add(items, "citron", 17)
+    add(items, "croissant", 23)
+    add(items, "framboise", 47)
+    add(items, "noisette", 20)
+    add(items, "the", 10)
 
     scene.onAnimation = (time: number) => {
-        for (let k=0 ; k<bowls.length ; k++) {
-            const bowl = bowls[k]
-            const speed1 = 0.001 * Math.sin(k)
-            const speed2 = 0.001 * Math.cos(2 * k)
-
-            const radius = (1 + Math.cos(time * (0.00147 + speed1))) * 100 + 50
-            const angle = time * (0.00247 + speed2)
+        for (let k=0 ; k<items.length ; k++) {
+            const item = items[k]
+            const speed1 = item.extra.speed1
+            const speed2 = item.extra.speed2
+            const radius = (1 + Math.cos(time * speed1)) * 120 + 10
+            const angle = time * speed2
             const x = 500 + radius * Math.cos(angle)
             const y = 500 + radius * Math.sin(angle)
+            const z = radius / 500
 
-            bowl.update({ x, y, scale: (radius + 250) / 500 })
+            item.update({ x, y, z, scale: (radius + 250) / 1000 })
         }
     }
     scene.start()
