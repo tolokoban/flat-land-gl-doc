01c81fb63f8b9459b491d5eb70c1d56cb223403f
Tolokoban <fabien.petitjean@epfl.ch>
2019-11-11 15:47:16 +0100

    Updates

---
+++ precache-manifest.9868d85acbe1177d3535ecb9c1fff096.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332440b5928a021c122768046ccc7920",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a21ae6c8d7536034adc5",
+    "url": "./static/css/index.58b687bf.3acec90f.chunk.css"
+  },
+  {
+    "revision": "979c5be6718679a0bc58",
+    "url": "./static/css/index.731f7d27.01bfa036.chunk.css"
+  },
+  {
+    "revision": "85b1a382abee769e2fe4",
+    "url": "./static/css/index.85478ee2.71184cb6.chunk.css"
+  },
+  {
+    "revision": "0fa17a826314d12105e7",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2840140dae19cefd23ae",
+    "url": "./static/js/0.754736d4.chunk.js"
+  },
+  {
+    "revision": "80c5621bcf2657f44e34",
+    "url": "./static/js/1.1da77bf4.chunk.js"
+  },
+  {
+    "revision": "a21ae6c8d7536034adc5",
+    "url": "./static/js/index.58b687bf.a77fa7b6.chunk.js"
+  },
+  {
+    "revision": "979c5be6718679a0bc58",
+    "url": "./static/js/index.731f7d27.379f5c84.chunk.js"
+  },
+  {
+    "revision": "85b1a382abee769e2fe4",
+    "url": "./static/js/index.85478ee2.967b7799.chunk.js"
+  },
+  {
+    "revision": "0fa17a826314d12105e7",
+    "url": "./static/js/main.15cb8975.chunk.js"
+  },
+  {
+    "revision": "9f83267410c42ffa8d85",
+    "url": "./static/js/runtime-index.58b687bf.a73a0440.js"
+  },
+  {
+    "revision": "d77847b59b31a2c646c1",
+    "url": "./static/js/runtime-index.731f7d27.a5b4b72f.js"
+  },
+  {
+    "revision": "5d3fa59adc1863a5c8da",
+    "url": "./static/js/runtime-index.85478ee2.7eb684df.js"
+  },
+  {
+    "revision": "aa85cead787c205e54c1",
+    "url": "./static/js/runtime-main.999881b1.js"
+  },
+  {
+    "revision": "086dcaef6afa84ebf4d12a77af999066",
+    "url": "./static/media/background.086dcaef.jpg"
+  },
+  {
+    "revision": "0935ef3449b4dbd252fb283b30808c78",
+    "url": "./static/media/background.0935ef34.png"
+  },
+  {
+    "revision": "9fe5ee102ce3698a98f0c86abc09e8a4",
+    "url": "./static/media/bowl.9fe5ee10.png"
+  }
+]);
